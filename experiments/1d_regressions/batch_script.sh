043da992b05db2725f3b67792cf2cc4a6df76b06
Sheev13 <rochussentn@gmail.com>
2025-07-24 17:58:50 +0200

    fully functioning gpu-accelerated batch job

--- experiments/1d_regressions/batch_script.sh
+++ experiments/1d_regressions/batch_script.sh
@@ -3,8 +3,9 @@
 #SBATCH --partition=gpu_p
 #SBATCH --qos=gpu_priority
 #SBATCH --mem=64G
+#SBATCH --constraint="[a100_80gb|h100_80gb]"
 #SBATCH --cpus-per-task=4
-#SBATCH --time=6:00:00
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 
@@ -16,7 +17,7 @@
 #SBATCH --mail-type=ALL
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate torch-sandbox
+conda activate bdnp-environment
 
 # Run your script with arguments
 python experiments/1d_regressions/1d_regressions.py --architecture 500 500 500 --learning_rate 1e-4 --final_learning_rate 1e-5 --train_new_model --use_gpu
